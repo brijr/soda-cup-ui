0cf1c343aee9f2cbb7e1450968cc3cb1f8cc7aab
Bridger Tower <me@brijr.dev>
2022-06-14 22:24:59 -0600

    update

---
+++ src/components/about-section-w-quote.svelte
@@ -0,0 +1,12 @@
+<section class="px-8 py-16">
+    <h3 class="text-3xl pb-4 font-bold">More about the Company</h3>
+    <div class="flex pb-4">
+        <img class="w-32 h-auto object-cover rounded-full my-4" src="https://ualr.edu/studentaffairs/files/2020/01/blank-picture-holder.png" alt="Placeholder">
+        <img class="w-32 h-auto object-cover rounded-full m-4" src="https://ualr.edu/studentaffairs/files/2020/01/blank-picture-holder.png" alt="Placeholder">
+    </div>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    <div class="pl-4 border-l-2 border-red-500 my-8">
+        <p class="text-xl">"A great review of the Service!"</p>
+        <p>– Jared Whitmore, Pheonix AZ</p>
+    </div>
+</section>
